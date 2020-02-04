866a66f5fe00e69469c97a8cdbbfd5d1f441a260
benGrowSquares <33531512+bmeyer19@users.noreply.github.com>
2020-02-03 17:56:45 -0800

    Initial Commit

---
+++ DKCodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DKCodingChallenge
+//
+//  Created by Ben Meyer on 2/3/20.
+//  Copyright © 2020 bmeyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
